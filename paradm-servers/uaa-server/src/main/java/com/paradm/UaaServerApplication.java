d3beb073371b46ed7fa2e4d45c63b09b1b104d49
Jackyshen <Jackyshen@Jackyshen-PC>
2018-05-18 00:46:48 +0800

    uaa-service

---
+++ paradm-servers/uaa-server/src/main/java/com/paradm/UaaServerApplication.java
@@ -0,0 +1,14 @@
+package com.paradm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UaaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UaaServerApplication.class, args);
+  }
+}
