fcd1dc1b0f1d1d67c3fe446e96a76b2de9a8f66b
Jackyshen <Jackyshen@Jackyshen-PC>
2018-05-18 01:23:28 +0800

    test

--- paradm-servers/uaa-service/src/main/java/com/paradm/service/UserService.java
+++ paradm-servers/uaa-service/src/main/java/com/paradm/service/UserService.java
@@ -1,17 +1,22 @@
 package com.paradm.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.paradm.repository.UserRepository;
+
 @Service
 public class UserService implements UserDetailsService {
 
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // TODO Auto-generated method stub
-    return null;
+    return userRepository.findByUsername(username);
   }
 
 }
