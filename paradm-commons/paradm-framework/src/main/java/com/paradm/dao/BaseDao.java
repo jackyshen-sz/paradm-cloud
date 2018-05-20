482f51f8344dc60e782f6d81718ff7b6331cfb96
Jackyshen <Jackyshen@Jackyshen-PC>
2018-05-20 15:29:34 +0800

    dao

---
+++ paradm-commons/paradm-framework/src/main/java/com/paradm/dao/BaseDao.java
@@ -0,0 +1,11 @@
+package com.paradm.dao;
+
+import java.io.Serializable;
+
+import com.paradm.entity.BaseEntity;
+import com.paradm.model.BaseModel;
+
+public interface BaseDao<I extends Serializable, T extends BaseEntity<I>, TE extends BaseModel<?>> {
+
+  int deleteByExample(TE model);
+}
